958ead284a749effc57cdd470fafa029f9684eb3
ayman <aymanmohamed1142001@gmail.com>
2026-08-21 21:46:53 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const tuneApp());
+}
+
+class tuneApp extends StatelessWidget {
+  const tuneApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
